5029bf0f33fa243488afe6524e043a4d9c5cb4cc
conoybhda <2022302181208@whu.edu.cn>
2023-11-07 00:03:55 +0800

    feat: 首页素材

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,21 @@
 <template>
-  <div>init</div>
-  <Hello />
+  <div class="index">
+    <img class="title" src="/source/index/title.png" />
+  </div>
 </template>
 
-<script setup>
+<script setup></script>
 
-</script>
-
-<style lang="stylus" scoped></style>
+<style scoped>
+.index{
+  width: 100vw;
+  height: 100vh;
+  overflow: hidden;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.title {
+  height: 100%;
+}
+</style>
