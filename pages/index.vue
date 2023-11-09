eea600d148bb2a8995567d803ba6f27081b21ee6
Little Weak_Duck <yinjiab@gmail.com>
2023-11-09 17:55:46 +0800

    feat: 完善动画，添加px2vw

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="index">
+    <div class="mask"></div>
     <img class="title" src="/source/PageIndex/title.png" />
     <img class="draw" src="/source/PageIndex/Draw.png" />
   </div>
@@ -18,139 +19,196 @@
   flex-direction: column;
   position: relative;
 }
+
 .title {
   height: 100%;
   object-fit: cover;
-  clip-path: polygon(0 0, 0 0, 0 0, 0 0); /* 开始时遮罩覆盖整个图像 */
-  animation: reveal 5s forwards ease-in-out; /* 定义动画时间和类型 */
+  z-index: -1;
+  /* 定义动画时间和类型 */
 }
+
 .draw {
   position: absolute;
   z-index: 100;
-  height: 300px;
+  width: 895px;
   align-self: flex-start;
-  animation: move 6s forwards ease-in-out;
+  animation: move 5s forwards ease-in-out;
   /* 设置元素开始时在左中间 */
   top: 50%;
   left: 0;
-  transform: translateY(-50%); /* 保持元素垂直居中 */
+  transform: translateY(-50%);
+  /* 保持元素垂直居中 */
 }
 
 @keyframes move {
   0% {
     top: 50%;
-    left: 0; /* 从左中间开始 */
+    left: 0;
+    /* 从左中间开始 */
     transform: translateY(-50%);
   }
+
   5% {
     top: 10%;
     left: 10%;
     transform: translateY(-50%);
   }
-  10% {
+
+  /* 10% {
     top: 90%;
-    left: 5%; /* 移动到顶部中间 */
+    left: 5%;
     transform: translateY(-50%);
   }
+
   15% {
     top: 10%;
     left: 15%;
     transform: translateY(-50%);
-  }
+  } */
+
   20% {
     top: 90%;
-    left: 10%; /* 移动到顶部中间 */
+    left: 10%;
     transform: translateY(-50%);
   }
-  25% {
+
+  /* 25% {
     top: 10%;
     left: 25%;
     transform: translateY(-50%);
   }
+
   30% {
     top: 90%;
-    left: 20%; /* 移动到顶部中间 */
+    left: 20%;
     transform: translateY(-50%);
-  }
+  } */
+
   35% {
     top: 10%;
     left: 35%;
     transform: translateY(-50%);
   }
-  40% {
+
+  /* 40% {
     top: 90%;
-    left: 30%; /* 移动到顶部中间 */
+    left: 30%;
     transform: translateY(-50%);
   }
+
   45% {
     top: 10%;
     left: 45%;
     transform: translateY(-50%);
-  }
+  } */
+
   50% {
     top: 90%;
-    left: 40%; /* 移动到顶部中间 */
+    left: 40%;
     transform: translateY(-50%);
   }
-  55% {
+
+  /* 55% {
     top: 10%;
     left: 55%;
     transform: translateY(-50%);
   }
+
   60% {
     top: 90%;
-    left: 50%; /* 移动到顶部中间 */
+    left: 50%;
     transform: translateY(-50%);
-  }
+  } */
+
   65% {
     top: 10%;
     left: 65%;
     transform: translateY(-50%);
   }
+
+  /* 
   70% {
     top: 90%;
-    left: 60%; /* 移动到顶部中间 */
+    left: 60%;
     transform: translateY(-50%);
   }
+
   75% {
     top: 10%;
     left: 75%;
     transform: translateY(-50%);
-  }
+  } */
+
   80% {
     top: 90%;
-    left: 70%; /* 移动到顶部中间 */
+    left: 70%;
     transform: translateY(-50%);
   }
-  85% {
+
+  /* 85% {
     top: 10%;
     left: 85%;
     transform: translateY(-50%);
   }
+
   90% {
     top: 90%;
-    left: 80%; /* 移动到顶部中间 */
+    left: 80%;
     transform: translateY(-50%);
-  }
+  } */
+
   95% {
     top: 10%;
     left: 95%;
     transform: translateY(-50%);
   }
+
   100% {
     top: 50%;
-    left: 100%; /* 移动到顶部中间 */
+    left: 100%;
+
     transform: translateY(-50%);
   }
 }
 
-/* 当.draw元素动画开始时，更新图片的clip-path */
-@keyframes reveal {
+.mask {
+  position: absolute;
+  width: 100%;
+  /* 遮罩宽度与容器相同 */
+  height: 100%;
+  /* 遮罩高度与容器相同 */
+  background: transparent;
+  /* 透明背景 */
+  animation: slide-right 5s forwards ease-in-out;
+  /* 定义动画时间和类型 */
+}
+
+.mask::after {
+  content: '';
+  position: absolute;
+  top: 0;
+  left: -10%;
+  /* 初始化时在容器左侧外 */
+  width: 100%;
+  /* 伪元素的宽度与容器相同 */
+  height: 100%;
+  /* 伪元素的高度与容器相同 */
+  background: #fff;
+  /* 遮罩颜色 */
+  transform: skewX(-20deg);
+  /* 倾斜变换 */
+  transform-origin: bottom right;
+  /* 变换的原点 */
+}
+
+/* 定义动画 */
+@keyframes slide-right {
   0% {
-    clip-path: polygon(0 0, 0 0, 0 0, 0 0);
+    transform: translateX(0%);
   }
+
   100% {
-    clip-path: polygon(0 0, 100% 0, 100% 100%, 0 100%);
+    transform: translateX(100%);
   }
 }
 </style>
